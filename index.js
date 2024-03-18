52d1ae749afbdee04559e5cad3befd2039d82cf5
Abhay Singh <singhchirag188@gmail.com>
2024-03-18 19:00:21 +0530

    Allowed user to edit any existing post. Allowed user to delete any post.

--- index.js
+++ index.js
@@ -2,6 +2,10 @@ const exp = require("constants");
 const express = require("express");
 const app = express();
 const path = require("path");
+const { v4: uuidv4 } = require('uuid');
+var methodOverride = require('method-override');
+
+app.use(methodOverride('_method'));
 
 let port = 3000;
 app.listen(port, () => {
@@ -18,19 +22,18 @@ app.use(express.static(path.join(__dirname,"public")));
 
 let posts = [
     {
-        id: "1",
+        id: uuidv4(),
         username: "Rahul Sharma",
         title: "Fav Language",
         content: "My favorite language is Python",
     }, 
     {
-        id: "2",
+        id: uuidv4(),
         username: "Abhay Rawat",
         title: "My Interests",
         content: "I love to read and write",
     }
 ];
-let lastid = 2;
 
 app.get("/posts",(req,res)=>{
     res.render("index",{posts});
@@ -39,8 +42,7 @@ app.get("/posts",(req,res)=>{
 app.post("/posts",(req,res)=>{
     let {username,title,content} = req.body;
     content = content.replace(/\n/g, "<br>");
-    lastid++;
-    posts.push({id: lastid.toString(),username,title,content});
+    posts.push({id: uuidv4(),username,title,content});
     res.redirect("/posts");
 });
 app.get("/posts/new",(req,res)=>{
@@ -56,6 +58,31 @@ app.get("/posts/:id",(req,res)=>{
     else{
         res.render("show",{post});
     }
+});
+app.get("/posts/:id/edit",(req,res)=>{
+    let {id} = req.params;
+    let post = posts.find(p => p.id === id);
+
+    if(post===undefined){
+        res.send("No such Post found");
+    }
+    else{
+        res.render("edit",{post});
+    }
+});
+app.patch("/posts/:id",(req,res)=>{
+    let content = req.body.content;
+    let id = req.params.id;
+    console.log(content);
+    //content = content.replace(/\n/g, "<br>");
+    let post = posts.find(p => p.id === id);
+    post.content = content;
+    res.redirect("/posts");
+});
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params;
+    posts = posts.filter(p => p.id !== id);
+    res.redirect("/posts");
 });
 
 
